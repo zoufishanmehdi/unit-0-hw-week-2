15b966eb44b6721d5b11d955609e749f56c23412
mikekavouras <kavourasm@gmail.com>
2015-06-21 19:02:37 -0400

    caesar

---
+++ CaesarCipher/CaesarCipher/main.m
@@ -0,0 +1,59 @@
+//
+//  main.m
+//  CaesarCipher
+//
+//  Created by Michael Kavouras on 6/21/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CaesarCipher : NSObject
+
+- (NSString *)decode:(NSString *)string offset:(int)offset;
+- (NSString *)encode:(NSString *)string offset:(int)offset;
+
+@end
+
+@implementation CaesarCipher
+
+- (NSString *)encode:(NSString *)string offset:(int)offset {
+    unsigned long count = [string length];
+    unichar result[count];
+    unichar buffer[count];
+    [string getCharacters:buffer range:NSMakeRange(0, count)];
+    
+    for (int i = 0; i < count; i++) {
+        result[i] = buffer[i];
+        if (buffer[i] == ' ') {
+            result[i] = ' ';
+            continue;
+        }
+        char c;
+        
+        int low = islower(buffer[i]) ? 'a' : 'A';
+        int high = islower(buffer[i]) ? 'z' : 'Z';
+        BOOL wrap = buffer[i] + offset > high || buffer[i] + offset < low;
+        
+        c = wrap ? ((buffer[i] + offset) % high) + (low - 1) : buffer[i] + offset;
+        
+        result[i] = c;
+    }
+    
+    return [NSString stringWithCharacters:result length:count];
+}
+
+- (NSString *)decode:(NSString *)string offset:(int)offset {
+    return [self encode:string offset:26 - offset];
+}
+
+@end
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+       
+        
+        
+    }
+    return 0;
+}
