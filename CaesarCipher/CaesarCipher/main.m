17b7d4b33a2d508bce0820f3ef459a2af97d9b2e
mikekavouras <kavourasm@gmail.com>
2015-06-22 17:36:38 -0400

    update

--- CaesarCipher/CaesarCipher/main.m
+++ CaesarCipher/CaesarCipher/main.m
@@ -7,13 +7,15 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "CaesarCipher.h"
 
 @interface CaesarCipher : NSObject
 
-- (NSString *)decode:(NSString *)string offset:(int)offset;
 - (NSString *)encode:(NSString *)string offset:(int)offset;
+- (NSString *)decode:(NSString *)string offset:(int)offset;
 
 @end
+
 
 @implementation CaesarCipher
 
@@ -21,19 +23,25 @@
     if (offset > 25) {
         NSAssert(offset < 26, @"offset is out of range. 1 - 25");
     }
+    NSString *str = [string lowercaseString];
     unsigned long count = [string length];
     unichar result[count];
     unichar buffer[count];
-    [string getCharacters:buffer range:NSMakeRange(0, count)];
+    [str getCharacters:buffer range:NSMakeRange(0, count)];
+    
+    char allchars[] = "abcdefghijklmnopqrstuvwxyz";
 
     for (int i = 0; i < count; i++) {
         if (buffer[i] == ' ' || ispunct(buffer[i])) {
             result[i] = buffer[i];
             continue;
         }
+        
+        char *e = strchr(allchars, buffer[i]);
+        int idx= (int)(e - allchars);
+        int new_idx = (idx + offset) % strlen(allchars);
 
-        int low = islower(buffer[i]) ? 'a' : 'A';
-        result[i] = (buffer[i]%low + offset)%26 + low;
+        result[i] = allchars[new_idx];
     }
 
     return [NSString stringWithCharacters:result length:count];
@@ -42,10 +50,12 @@
 - (NSString *)decode:(NSString *)string offset:(int)offset {
     return [self encode:string offset: (26 - offset)];
 }
+
 @end
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-
+        
     }
 }
